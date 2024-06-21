5eab6ae1480663da8fd16db33bfb7c287fd900ea
MartinPettersen <searchingtheskyfor@gmail.com>
2024-06-21 14:31:06 +0200

    add discover movies

--- components/(home)/HomePage.tsx
+++ components/(home)/HomePage.tsx
@@ -1,32 +1,66 @@
-import React from 'react'
-import { View, Text, StyleSheet, StatusBar } from 'react-native'
-import HomeContainer from './HomeContainer'
-import GenresContainer from '../(genre)/GenresContainer'
-import PopularContainer from '../(popular)/PopularContainer'
-import { useGetTopRatedMovies } from '../../hooks/useGetTopRatedMovies'
-import TopRatedContainer from '../(toprated)/TopRatedContainer'
+import React from "react";
+import { View, Text, StyleSheet, StatusBar, ScrollView } from "react-native";
+import HomeContainer from "./HomeContainer";
+import GenresContainer from "../(genre)/GenresContainer";
+import PopularContainer from "../(popular)/PopularContainer";
+import { useGetTopRatedMovies } from "../../hooks/useGetTopRatedMovies";
+import TopRatedContainer from "../(toprated)/TopRatedContainer";
+import { useGetDiscoverMovies } from "../../hooks/useGetDiscoverMovies";
+import MovieListContainer from "./MovieListContainer";
 
 const HomePage = () => {
-
+  const discoverMovies = useGetDiscoverMovies();
 
   return (
     <View style={styles.container}>
-        <PopularContainer />
+      <PopularContainer />
+      <ScrollView>
+      <View style={styles.section}>
         <TopRatedContainer />
+      </View>
+        <View style={styles.section}>
+          <MovieListContainer
+            headline="Oppdag Spennende Nyheter innen Filmverdenen"
+            text="Opplev De Nyeste Filmene Nå"
+            movies={discoverMovies}
+          />
+        </View>
+
+        <View style={styles.section}>
+          <MovieListContainer
+            headline="Oppdag Spennende Nyheter innen Filmverdenen"
+            text="Opplev De Nyeste Filmene Nå"
+            movies={discoverMovies}
+          />
+        </View>
+        <View style={styles.section}>
+          <MovieListContainer
+            headline="Oppdag Spennende Nyheter innen Filmverdenen"
+            text="Opplev De Nyeste Filmene Nå"
+            movies={discoverMovies}
+          />
+        </View>
+      </ScrollView>
     </View>
-  )
-}
+  );
+};
 
-const styles= StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-        paddingTop: 50,
-   },
-   text:{
-    color: 'white'
-   }
-})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    paddingTop: 50,
+  },
+  text: {
+    color: "white",
+  },
+  section: {
+    flex: 1,
+    width: "100%",
+    paddingHorizontal: 10,
+    marginBottom: 20,
+  },
+});
 
-export default HomePage
+export default HomePage;
