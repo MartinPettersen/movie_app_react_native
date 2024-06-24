906850bb3e2aa2094a7b8cc0a403d4187adf54bc
MartinPettersen <searchingtheskyfor@gmail.com>
2024-06-24 09:26:19 +0200

    fix bug with white background when scrolling

--- App.tsx
+++ App.tsx
@@ -5,21 +5,25 @@ import Loading from "./components/(loading)/Loading";
 import { usefetchMovieDBData } from "./hooks/useGetMovieDBData";
 import HomeScreen from "./screens/HomeScreen";
 import Tabs from "./Tabs";
-import { NavigationContainer } from "@react-navigation/native";
+import { DarkTheme, NavigationContainer } from "@react-navigation/native";
+import TabsNavigation from "./Tabs";
 
 export default function App() {
   // usefetchMovieDBData()
 
   return (
-    <NavigationContainer>
-        <Tabs />
-        <StatusBar style="auto" />
-    </NavigationContainer>
+<View style={styles.container}>
+      <NavigationContainer theme={DarkTheme}>
+        <TabsNavigation />
+      </NavigationContainer>
+      <StatusBar style="light" />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  background: {
+  container: {
+    flex: 1,
     backgroundColor: "black"
   }
 });
